99febb1d00e92e1fa91c02b90193f06140ca8969
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2012-07-10 23:21:31 +0200

    Progress on the TVDbEpisode class.

---
+++ TVDb/TVDbEpisode.h
@@ -0,0 +1,28 @@
+//
+//  TVDbEpisode.h
+//  iTVDb
+//
+//  Created by Kevin Tuhumury on 7/10/12.
+//  Copyright (c) 2012 Thmry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TVDbEpisode : NSObject
+
+@property (nonatomic, retain) NSNumber *episodeId;
+@property (nonatomic, retain) NSString *title;
+@property (nonatomic, retain) NSString *description;
+@property (nonatomic, retain) NSNumber *seasonNumber;
+@property (nonatomic, retain) NSNumber *episodeNumber;
+@property (nonatomic, retain) NSString *banner;
+@property (nonatomic, retain) NSString *director;
+@property (nonatomic, retain) NSString *writer;
+@property (nonatomic, retain) NSDate   *airDate;
+@property (nonatomic, retain) NSNumber *rating;
+@property (nonatomic, retain) NSString *imdbId;
+
++ (TVDbEpisode *)findById:(NSNumber *)id;
++ (TVDbEpisode *)findByShowId:(NSNumber *)showId seasonNumber:(NSNumber *)seasonNumber EpisodeNumber:(NSNumber *)episodeNumber;
+
+@end
---
+++ TVDb/TVDbEpisode.m
@@ -0,0 +1,25 @@
+//
+//  TVDbEpisode.m
+//  iTVDb
+//
+//  Created by Kevin Tuhumury on 7/10/12.
+//  Copyright (c) 2012 Thmry. All rights reserved.
+//
+
+#import "TVDbEpisode.h"
+
+@interface TVDbEpisode()
+
++ (NSString *)episodesUrl:(NSNumber *)showId;
+
+@end
+
+
+@implementation TVDbEpisode
+
++ (NSString *)episodesUrl:(NSNumber *)showId
+{
+    return [[[TVDbClient sharedInstance] apiKey] stringByAppendingString: [NSString stringWithFormat: @"/series/%@/all/", showId]];
+}
+
+@end
